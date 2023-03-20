9dc94958fcc093bd24389dc2d97a8bcc5834d18b
PyotSawe <potsaw01@gmail.com>
2023-03-21 01:36:16 +0300

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	for (; num <= 63; num++)
+	{
+		if (num > 57)
+		{
+			putchar(num + 39);
+		}
+		else
+			putchar(num);
+	}
+	return (0);
+}
