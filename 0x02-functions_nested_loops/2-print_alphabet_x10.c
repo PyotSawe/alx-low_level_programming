c048010b692ae3ad37ef53c6bba9f25ae8175362
PyotSawe <potsaw01@gmail.com>
2023-03-23 01:20:11 +0300

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+/**
+ * print_alphabet_x10 - print lowercase alphabets ten times
+ */
+void print_alphabet_x10(void)
+{
+	int ntimes;
+
+	for (ntimes = 0; ntimes <= 10; ++ntimes)
+	{
+		print_alphabet();
+	}
+	_putchar('\n');
+}
