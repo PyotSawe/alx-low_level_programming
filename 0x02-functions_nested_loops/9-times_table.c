7a465f01cf350aba3aea596d749cefa7c76f6f5b
PyotSawe <potsaw01@gmail.com>
2023-03-23 08:35:34 +0300

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * times_table - print multiplication table
+ */
+
+voiid times_table(void)
+{
+	int x;
+	int y;
+	int product;
+
+	for (y = 0; y < 10; ++y)
+	{
+		for (x = 0; x < 10; ++x)
+		{
+			product = (x * y);
+			if (product == 0)
+			{
+				_putchar('0' + product);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (product < 10)
+			{
+				_putchar('0' + product);
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+			else if (product > 10)
+			{
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
