e546f52c3429538494200241ade338d185b3f9ac
PyotSawe <potsaw01@gmail.com>
2023-03-25 00:42:02 +0300

    Fixed 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,8 @@ void more_numbers(void)
 		for (num = 0; num <= 14; ++num)
 		{
 			if (num >= 10)
-			{
-				_putchar('0' + (num / 10));
-				_putchar('0' + (num % 10));
-			}
-			else
-				_putchar('0' + num);
+				_putchar('1');
+			_putchar ('0' + num % 10);
 		}
 		_putchar('\n');
 	}
