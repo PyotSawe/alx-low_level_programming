d567b03679201db1cab99f49d3b35eb70fa3843e
PyotSawe <potsaw01@gmail.com>
2023-03-25 00:26:17 +0300

    Added and Fixed implementation file of void more_numbers(void);

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,9 @@ void more_numbers(void)
 	{
 		for (num = 0; num <= 14; ++num)
 		{
-			if (num > 9)
-			{
-				_putchar('0' + (num / 10));
-				_putchar('0' + (num % 10));
-			}
-			else
-				_putchar('0' + num);
+			if (num >= 10)
+				_putchar('0' + 1);
+			_putchar (num % 10 + '0');
 		}
 		_putchar('\n');
 	}
