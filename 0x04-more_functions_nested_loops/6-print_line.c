43825959427a25408ff8da827d785c0787ec29c0
PyotSawe <potsaw01@gmail.com>
2023-03-25 00:45:28 +0300

    Added an implementation file for draing lines void print_line(int n);

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - Function that prints seq of ('_') n times
+ *
+ * @n: number times the '_' should be printed
+ *
+ * Return: Does not return anything
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int count;
+
+		for (count = 0; count <= n; ++count)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
