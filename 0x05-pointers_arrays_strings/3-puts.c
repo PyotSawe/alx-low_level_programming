41511ed84bb5f4a5b6d8d19912ac5cbf7e4c6128
PyotSawe <potsaw01@gmail.com>
2023-03-28 06:57:12 +0300

    Fixed 3-puts.c finally

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,16 +7,11 @@
 
 void _puts(char *str)
 {
-	if (*str == '\0')
-		_putchar(*str);
-	else
-	{
-		int char_pos;
+	int char_pos;
 
-		for (char_pos = 0; str[char_pos] != '\0'; char_pos++)
-		{
-			_putchar(str[char_pos]);
-		}
-		_putchar('\n');
+	for (char_pos = 0; str[char_pos] != '\0'; char_pos++)
+	{
+		_putchar(str[char_pos]);
 	}
+	_putchar('\n');
 }
