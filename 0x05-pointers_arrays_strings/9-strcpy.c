7d3bd9c4c1e46f0ea5f63aac3c0436763e55d8aa
PyotSawe <potsaw01@gmail.com>
2023-03-28 06:29:26 +0300

    Added Impl file for char *_strcpy(char *dest, char *src);

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest.
+ * @dest: Destination to copy to
+ * @src: Src of string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *copied = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		++src;
+		++dest;
+	}
+	return (copied);
+}
