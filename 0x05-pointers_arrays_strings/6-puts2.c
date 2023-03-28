25b2795340e8ed462644fd18bb371b6332ca022e
PyotSawe <potsaw01@gmail.com>
2023-03-28 05:38:28 +0300

    Added missing @str Documentation for 6-puts.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,6 +3,8 @@
 /**
  * puts2 -  prints every other character of a string,
  * starting with the first character, followed by a new line.
+ *
+ * @str: String to print every char
  */
 
 void puts2(char *str)
