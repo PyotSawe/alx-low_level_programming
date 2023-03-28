83589934cf7debef90054d86fdfbfa77f8b7b95c
PyotSawe <potsaw01@gmail.com>
2023-03-28 06:50:43 +0300

    Added Impl file for int _atoi(char *s);

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a given str into an integer
+ *
+ * @s: the string to convert
+ *
+ * Return: converted  int
+ */
+
+int _atoi(char *s)
+{
+	int head_sign = 1;
+	int i = 0;
+	int converted = 0;
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			head_sign *= -1;
+		i++;
+	}
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{
+		converted = (converted * 10) + (s[i] - '0');
+		i++;
+	}
+	converted *= head_sign;
+	return (converted);
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 /**
- * _strcpy - copies the string pointed to by src,
+ * *_strcpy - copies the string pointed to by src,
  * including the terminating null byte (\0), to the buffer pointed to by dest.
  * @dest: Destination to copy to
  * @src: Src of string
