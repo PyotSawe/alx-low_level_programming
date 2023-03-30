9ea4177462ee0db39724d3821cebd82aa0771f35
PyotSawe <potsaw01@gmail.com>
2023-03-30 23:08:47 +0000

    Added program file for 102-magic.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,29 +4,30 @@
  * Return: An encode string
  */
 
-char *rot13(char *msg)
+char *rot13(char *s)
 {
-	int msg_i = 0;
-
-	/*Repeat getting a character from str until encounter null characters*/
-	while (*(msg + msg_i) != '\0')
+	/**/
+	char rot13enc[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+	char alphabet[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+			     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+			     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+			     'W', 'X', 'Y', 'Z'};
+	int shared_i, s_index;
+	for (s_index = 0; *(s + s_index) != '\0'; s_index++) /*loop through string*/
 	{
-		/*Repeat checking the character*/
-		while ((*(msg + msg_i) >= 'a' && *(msg + msg_i) <= 'z') ||
-				(*(msg + msg_i) >= 'A' && *(msg + msg_i) <= 'Z'))
-		{
-			/*If its between a - m or A - M */
-			if ((*(msg + msg_i) >= 'a' && *(msg + msg_i) <= 'm') ||
-					(*(msg + msg_i) >= 'A' && *(msg + msg_i) <= 'M'))
-				/*Then encode character to rot13*/
-				*(msg + msg_i) += 13;
-			else
-				/*Otherwise decode it*/
-				*(msg + msg_i) -= 13;
-			msg_i++;
-		}
-		msg_i++;
+		shared_i = 0;
+		while (alphabet[shared_i] != '\0' && *(s + s_index) != alphabet[shared_i]) /*loop through rot13 arr*/
+			shared_i++;
+
+		if (*(s + s_index) == alphabet[shared_i]) /*if alpha matches, set to index in b arr*/
+			*(s + s_index) = rot13enc[shared_i];
 	}
+
 	/*There exist other way to use rot13[53] array*/
-	return (msg);
+	return (s);
 }
