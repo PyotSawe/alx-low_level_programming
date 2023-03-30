47b5b37e64b37262804e77205f077f7642407ba9
PyotSawe <potsaw01@gmail.com>
2023-03-30 21:59:39 +0000

    Added Impl file for char *rot13(char *);

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+/**
+ * rot13 -  a   function that encodes a string using rot13 encoding scheme.
+ * @msg: An input string to encode using rot13
+ * Return: An encode string
+ */
+
+char *rot13(char *msg)
+{
+	int msg_i = 0;
+
+	/*Repeat getting a character from str until encounter null characters*/
+	while (*(msg + msg_i))
+	{
+		/*Repeat checking the character*/
+		while ((*(msg + msg_i) >= 'a' && *(msg + msg_i) <= 'z') ||
+				(*(msg + msg_i) >= 'A' && *(msg + msg_i) <= 'Z'))
+		{
+			/*If its between a - m or A - M */
+			if ((*(msg + msg_i) >= 'a' && *(msg + msg_i) <= 'm') ||
+					(*(msg + msg_i) >= 'A' && *(msg + msg_i) <= 'M'))
+				/*Then encode character to rot13*/
+				*(msg + msg_i) += 13;
+			else
+				/*Otherwise decode it*/
+				*(msg + msg_i) -= 13;
+			msg_i++;
+		}
+		msg_i++;
+	}
+	/*There exist other way to use rot13[53] array*/
+	return (msg);
+}
