1a9631059590b7bef13d46bed9741049f2896393
PyotSawe <potsaw01@gmail.com>
2023-03-31 00:39:58 +0000

    Fixed Impl file for char *rot13(char *);

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -4,30 +4,33 @@
  * Return: An encode string
  */
 
-char *rot13(char *s)
+char *rot13(char *msg)
 {
-	/**/
-	char rot13enc[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
-		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+	/*Prepare Encoding bytes and alphabets*/
+	char rot13enc[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 		     'J', 'K', 'L', 'M'};
-	char alphabet[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-			     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
-			     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-			     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-			     'W', 'X', 'Y', 'Z'};
-	int shared_i, s_index;
-	for (s_index = 0; *(s + s_index) != '\0'; s_index++) /*loop through string*/
+	char alphabet[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+		'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		'W', 'X', 'Y', 'Z'};
+	/*Mark where to start*/
+	int shared_i, msg_index;
+
+	/*For each msg character*/
+	for (msg_index = 0; *(msg + msg_index) != '\0'; msg_index++)
 	{
+		/*repeat correlating with rot13enc*/
 		shared_i = 0;
-		while (alphabet[shared_i] != '\0' && *(s + s_index) != alphabet[shared_i]) /*loop through rot13 arr*/
+		while (alphabet[shared_i] != '\0' && *(msg + msg_index)
+				!= alphabet[shared_i])
 			shared_i++;
-
-		if (*(s + s_index) == alphabet[shared_i]) /*if alpha matches, set to index in b arr*/
-			*(s + s_index) = rot13enc[shared_i];
+		/*encode msg char using codec char*/
+		if (*(msg + msg_index) == alphabet[shared_i])
+			*(msg + msg_index) = rot13enc[shared_i];
 	}
-
-	/*There exist other way to use rot13[53] array*/
-	return (s);
+	return (msg);
 }
