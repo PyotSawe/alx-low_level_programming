2f3007edddd3f2c53b112d3b3b14d3e212b7ab15
PyotSawe <potsaw01@gmail.com>
2023-04-20 01:16:05 +0000

    Added completed 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the opcodes of itself.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char const *argv[])
+{
+	unsigned char opcode;
+	int code_len, code_off;
+	int (*_entry)(int, char const**) = main;
+
+	if (argc != 2)
+	{
+		if (argc > 2)
+		{
+			printf("Error: Command Options MORE than 2\n");
+			exit(1);
+		}
+		else if (argc < 2)
+		{
+			printf("Error: Command Options LESS than 2\n");
+			exit(1);
+		}
+	}
+	code_len = atoi(argv[1]);
+	if (code_len < 0)
+	{
+		printf("Error: Bytes should be more than 0\n");
+		exit(2);
+	}
+	for (code_off = 0; code_off < code_len; code_off++)
+	{
+		opcode = *(unsigned char *)_entry;
+		printf("%.2x", opcode);
+		if (code_off < code_len)
+			putchar(' ');
+		_entry++;
+	}
+	printf("\n");
+	return (0);
+}
