8296e08e73cf5a5327f8ec2a9804819fde94c431
PyotSawe <potsaw01@gmail.com>
2023-04-29 04:42:23 +0300

    Added impl file for list_t *add_node(list_t **head, const char *str);

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: pointer to pointer to head
+ * @str: new string to add
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *added;
+	unsigned int len = 0;
+
+	while (str[++len])
+		;/*Calculate len of str*/
+	added = malloc(sizeof(list_t));
+	if (added == NULL)
+		return (NULL);/*don't proceed if malloc had error*/
+	/*Built the node*/
+	added->str = strdup(str);
+	added->len = len;
+	added->next = (*head);
+	(*head) = added;
+	return (*head);
+}
