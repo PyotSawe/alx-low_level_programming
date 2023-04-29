f61a095194cedf15b5c2a1e322ae40c2e5d14036
PyotSawe <potsaw01@gmail.com>
2023-04-29 03:39:40 +0300

    Fixed lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,18 +2,18 @@
 #define LISTS_H
 
 /**
- * struct List - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list
  */
-typedef struct List
+typedef struct list_s
 {
-		char *str;
-			unsigned int len;
-				struct List *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
